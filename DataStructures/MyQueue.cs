a369e7416a5c2629e9e60e2ead0d9b9b772284ee
Camrado <blackandwhite.b17w@gmail.com>
2024-09-09 19:56:15 +0400

    Implemented Queue

---
+++ DataStructures/MyQueue.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+
+namespace DataStructures;
+
+public class MyQueue<T> : IEnumerable<T> {
+    private LinkedList<T> _list = new();
+    
+    public MyQueue() {
+    }
+    
+    public MyQueue(T firstElem) {
+        Enqueue(firstElem);
+    }
+    
+    public int Size() {
+        return _list.Count;
+    }
+    
+    public bool IsEmpty() {
+        return Size() == 0;
+    }
+    
+    public T Peek() {
+        if (IsEmpty()) {
+            throw new InvalidOperationException("Queue is empty");
+        }
+
+        return _list.First();
+    }
+
+    public T Dequeue() {
+        if (IsEmpty()) 
+            throw new InvalidOperationException("Queue is empty");
+
+        T first = _list.First();
+        _list.RemoveFirst();
+        
+        return first;
+    }
+    
+    public void Enqueue(T elem) {
+        _list.AddLast(elem);
+    }
+    
+    public IEnumerator<T> GetEnumerator() {
+        return _list.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() {
+        return GetEnumerator();
+    }
+}
